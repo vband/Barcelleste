a7c2285982b3ee26b3af40a8a46439601ac0bd20
Vítor Barcellos <vtor.barcellos7@gmail.com>
2020-03-07 12:22:09 -0300

    Killable dies, killer gets knockback

---
+++ Assets/Barcelleste/Scripts/Components/Killable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Barcelleste
+{
+    public class Killable : MonoBehaviour
+    {
+        public void Die()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Barcelleste/Scripts/Components/Killer.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Barcelleste
+{
+    [RequireComponent(typeof(Collider2D))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Killer : MonoBehaviour
+    {
+        [SerializeField] private float verticalImpulseVelocity = 0;
+        [SerializeField] private float knockbackVelocity = 0;
+        [SerializeField] private float stunDuration = 0;
+
+        private new Rigidbody2D rigidbody;
+        private new Collider2D collider;
+
+        private void Start()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+            collider = GetComponent<Collider2D>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var killable = collision.gameObject.GetComponent<Killable>();
+            if (killable)
+            {
+                if (IsBelow(collision))
+                {
+                    killable.Die();
+                    ApplyVerticalImpulse();
+                }
+                else
+                {
+                    ApplyKnockback(collision);
+                }
+            }
+        }
+
+        private bool IsBelow(Collision2D collision)
+        {
+            for (int i = 0; i < collision.contactCount; i++)
+            {
+                if (collision.GetContact(i).point.y < collider.bounds.min.y)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void ApplyVerticalImpulse()
+        {
+            rigidbody.velocity = new Vector2
+            {
+                x = rigidbody.velocity.x,
+                y = verticalImpulseVelocity
+            };
+        }
+
+        private void ApplyKnockback(Collision2D collision)
+        {
+            var direction = (transform.position - collision.transform.position).normalized;
+            rigidbody.velocity = direction * knockbackVelocity;
+            StartCoroutine(DisableMovementFor(seconds: stunDuration));
+        }
+
+        private IEnumerator DisableMovementFor(float seconds)
+        {
+            var moveScript = GetComponent<MovingCharacter>();
+            var jumpScript = GetComponent<JumpingCharacter>();
+
+            if (moveScript) moveScript.enabled = false;
+            if (jumpScript) jumpScript.enabled = false;
+            yield return new WaitForSeconds(seconds);
+            if (moveScript) moveScript.enabled = true;
+            if (jumpScript) jumpScript.enabled = true;
+        }
+    }
+}
